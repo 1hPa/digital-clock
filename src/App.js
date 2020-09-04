4362c3d135d4f99a12d757d3622102f848f87c29
Yu <tuba0927hy@gmail.com>
2020-09-04 22:15:03 +0900

    style: formatting

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 class App extends Component {
     constructor(props){
-        super(props){
-        this.state = {
-            timelag : '',
-            nowTime : ''
-        }
+      super(props)
+      this.state = {
+        timelag : '',
+        nowTime : ''
     }
+  }
 }
 
 export default App;
