2e3572d92d81aa9217d217ced481a3ded4903911
Yu <tuba0927hy@gmail.com>
2020-09-04 22:12:59 +0900

    feat: add initial value

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React from 'react';
 class App extends Component {
     constructor(props){
         super(props){
+        this.state = {
+            timelag : '',
+            nowTime : ''
         }
     }
 }
